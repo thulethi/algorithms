f353ae9fc6e059d321cb78b439b75901dc01ad11
Thu Le <lethikhanhthu@gmail.com>
2018-11-19 16:05:41 +0100

    Generate random notes per chord

---
+++ random_notes_chord.rb
@@ -0,0 +1,7 @@
+require 'rspec'
+
+# Write a function which given a sequence of cords, e.g “d, G, CMaj, CMaj” generates a random melody
+# from notes of these chords, e.g. outputs “d f a, g d b, c e g, e g c”.
+# Let’s suppose it generates 3 notes per chord. Hint: ask for help.
+
+RSpec::Core::Runner.run([$__FILE__])
